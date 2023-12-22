a8527fba8015691fe74df698e1fc51b3cc23100e
KleinLiuZhikai <kleinlau17@gmail.com>
2023-12-22 18:22:14 +0800

    护盾制作

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -9,6 +9,10 @@ public class Health : MonoBehaviour
     [SerializeField] private float initialHealth = 10f;
     [SerializeField] private float maxHealth = 10f;
 
+    [Header("Shield")]
+    [SerializeField] private float initialShield = 5f;
+    [SerializeField] private float maxShield = 5f;
+
     [Header("Settings")]
     [SerializeField] private bool destroyObject;
 
@@ -17,8 +21,13 @@ public class Health : MonoBehaviour
     private Collider2D collider2D;
     private SpriteRenderer spriteRenderer;
 
+    private bool shieldBroken;
+
     // Controls the current health of the object    
     public float CurrentHealth { get; set; }
+
+    // Returns the current health of this character
+    public float CurrentShield { get; set; }
 
     private void Awake()
     {
@@ -28,7 +37,8 @@ public class Health : MonoBehaviour
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
 
         CurrentHealth = initialHealth;
-        UIManager.Instance.UpdateHealth(CurrentHealth, maxHealth);
+        CurrentShield = initialShield;
+        UIManager.Instance.UpdateHealth(CurrentHealth, maxHealth, CurrentShield, maxShield);
     }
 
     private void Update()
@@ -47,8 +57,20 @@ public class Health : MonoBehaviour
             return;
         }
 
+        if (!shieldBroken && character != null)
+        {
+            CurrentShield -= damage;
+            UIManager.Instance.UpdateHealth(CurrentHealth, maxHealth, CurrentShield, maxShield);
+
+            if (CurrentShield <= 0)
+            {
+                shieldBroken = true;
+            }
+            return;
+        }
+
         CurrentHealth -= damage;
-        UIManager.Instance.UpdateHealth(CurrentHealth, maxHealth);
+        UIManager.Instance.UpdateHealth(CurrentHealth, maxHealth, CurrentShield, maxShield);
 
         if (CurrentHealth <= 0)
         {
@@ -89,7 +111,11 @@ public class Health : MonoBehaviour
         gameObject.SetActive(true);
 
         CurrentHealth = initialHealth;
-        UIManager.Instance.UpdateHealth(CurrentHealth, maxHealth);
+        CurrentShield = initialShield;
+
+        shieldBroken = false;
+
+        UIManager.Instance.UpdateHealth(CurrentHealth, maxHealth, CurrentShield, maxShield);
     }
 
     // If destroyObject is selected, we destroy this game object
--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -9,25 +9,34 @@ public class UIManager : Singleton<UIManager>
 {
     [Header("Settings")]
     [SerializeField] private Image healthBar;
+    [SerializeField] private Image shieldBar;
     [SerializeField] private TextMeshProUGUI currentHealthTMP;
+    [SerializeField] private TextMeshProUGUI currentShieldTMP;
 
     private float playerCurrentHealth;
     private float playerMaxHealth;
+    private float playerMaxShield;
+    private float playerCurrentShield;
 
     private void Update()
     {
         InternalUpdate();
     }
 
-    public void UpdateHealth(float currentHealth, float maxHealth)
+    public void UpdateHealth(float currentHealth, float maxHealth, float currentShield, float maxShield)
     {
         playerCurrentHealth = currentHealth;
         playerMaxHealth = maxHealth;
+        playerCurrentShield = currentShield;
+        playerMaxShield = maxShield;
     }
 
     private void InternalUpdate()
     {
         healthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount, playerCurrentHealth / playerMaxHealth, 10f * Time.deltaTime);
         currentHealthTMP.text = playerCurrentHealth.ToString() + "/" + playerMaxHealth.ToString();
+
+        shieldBar.fillAmount = Mathf.Lerp(shieldBar.fillAmount, playerCurrentShield / playerMaxShield, 10f * Time.deltaTime);
+        currentShieldTMP.text = playerCurrentShield.ToString() + "/" + playerMaxShield.ToString();
     }
 }
