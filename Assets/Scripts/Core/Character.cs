3d0ec4127029ac2bfae66c2c1942be7f47992482
KleinLiuZhikai <kleinlau17@gmail.com>
2023-12-27 22:18:52 +0800

    武器转向优化

--- Assets/Scripts/Core/Character.cs
+++ Assets/Scripts/Core/Character.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,4 +13,25 @@ public class Character : MonoBehaviour
     [SerializeField] private CharacterTypes characterType;
 
     public CharacterTypes CharacterType => characterType;
+}*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public enum CharacterTypes
+    {
+        Player,
+        AI
+    }
+
+    [SerializeField] private CharacterTypes characterType;
+    [SerializeField] private GameObject characterSprite;
+    [SerializeField] private Animator characterAnimator;
+
+    public CharacterTypes CharacterType => characterType;
+    public GameObject CharacterSprite => characterSprite;
+    public Animator CharacterAnimator => characterAnimator;
 }
